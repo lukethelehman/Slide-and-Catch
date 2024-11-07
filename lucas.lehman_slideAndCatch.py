b9181fe3a7f6e039bb029a8daed43717d29c51f9
lukethelehman <lukelehman05@gmail.com>
2024-11-07 14:35:30 -0500

    Milestone 5

--- lucas.lehman_slideAndCatch.py
+++ lucas.lehman_slideAndCatch.py
@@ -2,6 +2,28 @@ import pygame, simpleGE, random
 
 
 
+
+class Cat(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("cat.png")
+        self.setSize(150, 150)
+        self.minSpeed = 3
+        self.maxSpeed = 8
+        self.reset()
+        
+    def reset(self):
+        self.y = 10
+        self.x = random.randint(0, self.screenWidth)
+        
+        self.dy = random.randint(self.minSpeed, self.maxSpeed)
+        
+    def checkBounds(self):
+        if self.bottom > self.screenHeight:
+            self.reset()
+        
+        
+
 class Basket(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
@@ -23,8 +45,10 @@ class Game(simpleGE.Scene):
         super().__init__()
         self.setImage("tree.png")
         self.basket = Basket(self)
+        self.cat = Cat(self)
         
-        self.sprites = [self.basket]
+        self.sprites = [self.basket,
+                        self.cat]
         
         
         
