a7311a798d1679b917ac2c37a91cf25dc3d7c462
lukethelehman <lukelehman05@gmail.com>
2024-11-09 16:22:40 -0500

    milestone 8

--- lucas.lehman_slideAndCatch.py
+++ lucas.lehman_slideAndCatch.py
@@ -30,7 +30,7 @@ class Basket(simpleGE.Sprite):
         self.setImage("basket.png")
         self.setSize(120,100)
         self.position = (140, 430)
-        self.moveSpeed = 10 
+        self.moveSpeed = 75
         
         
     def process(self):
@@ -86,14 +86,81 @@ class Game(simpleGE.Scene):
         if self.timer.getTimeLeft() < 0:
             print(f"Score: {self.score}")
             self.stop()
+            
+class Instructions(simpleGE.Scene):
+    def __init__(self, lastScore):
+        super().__init__()
+        self.setImage("tree.png")
+        self.response = "Quit"
+        self.prevScore = lastScore
+        self.directions = simpleGE.MultiLabel()
+        self.directions.textLines = [
+            "You are controlling a basket",
+            "Move with left and right arrow keys.",
+            "Catch as many cats as you can",
+            "in the time provided",
+            "",
+            "Good luck!"]
+        self.directions.center = (320,240)
+        self.directions.size = (500, 250)
+        
+        self.btnPlay = simpleGE.Button()
+        self.btnPlay.text = "Play"
+        self.btnPlay.center = (100,400)
+        
+        self.btnQuit = simpleGE.Button()
+        self.btnQuit.text = "Quit"
+        self.btnQuit.center = (530,400)
+        
+        self.lblScore = simpleGE.Label()
+        self.lblScore.text = "Last score: 0"
+        self.lblScore.center = (320, 400)
+        self.lblScore.text = f"Last score: {self.prevScore}"
+        
+        self.sprites = [self.directions,
+                        self.btnPlay,
+                        self.btnQuit,
+                        self.lblScore]
+        
+
+        
+        
+    def process(self):
+        if self.btnPlay.clicked:
+            self.response = "Play"
+            self.stop()
+        
+        if self.btnQuit.clicked:
+            self.response = "Quit"
+            self.stop()
+            
+    
                 
             
         
         
         
 def main():
-    game = Game()
-    game.start()
+    
+    keepGoing = True
+    lastScore = 0
+    while keepGoing:
+        
+        instructions = Instructions(lastScore)
+        instructions.start()
+        
+        if instructions.response == "Play":
+            game = Game()
+            game.start()
+            lastScore = game.score
+        
+        else:
+            keepGoing = False
+        
+    
+    
+    
+
 
 
 
